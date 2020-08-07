d0199e163b2d518f75f45a26b661123c5ccf13dd
Elian Becali <elianbecaliaguiar@gmail.com>
2020-08-07 09:02:24 -0400

    concluído front-end

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,36 +1,59 @@
 import React from 'react';
 
-
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/54561377?s=460&u=43e4954a86f8666ca7e21b7547312c92b0c3b2e0&v=4" alt="Elian Becali" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Elian Becali</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /><br />
-                Apaixonado por explodir coisas em laboratório e mudar de vida.
+                {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 450,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`} 
+                    target="_blank"
+                >
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
